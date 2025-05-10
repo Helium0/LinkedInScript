4aeaf96b8cd59d8589d4a5287422e2f344a6f2ef
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-10 23:50:46 +0200

    Added another locators and methods

--- src/test/java/pages/PersonsFinderPage.java
+++ src/test/java/pages/PersonsFinderPage.java
@@ -2,8 +2,13 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import projectHelper.WaitsGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonsFinderPage {
 
@@ -11,8 +16,15 @@ public class PersonsFinderPage {
     private WebDriver driver;
 
     @FindBy(xpath = "//a[text()='Zobacz wszystkie wyniki osób']")
-    private By seeAllPersonsButton;
+    private WebElement seeAllPersonsButton;
 
+    @FindBy(xpath = "//ul[@role='list']//li")
+    private List<WebElement> personsList;
+
+
+    public List<WebElement> getPersonsList() {
+        return personsList;
+    }
 
     public PersonsFinderPage (WebDriver driver) {
         PageFactory.initElements(driver,this);
@@ -20,6 +32,14 @@ public class PersonsFinderPage {
     }
 
 
+    public void clickOnAllPersonsButton(WebDriver driver) {
+        WaitsGenerator.waitForElement(driver,seeAllPersonsButton).click();
+    }
+
+
+    public void personsListWebelements() {
+        WaitsGenerator.waitForWebElements(driver,personsList);
+    }
 
 
 
