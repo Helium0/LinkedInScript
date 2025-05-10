c399658ff5c180c4737ca86bb869f18c757d02db
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-10 23:49:38 +0200

    Added more wait methods

--- src/test/java/projectHelper/WaitsGenerator.java
+++ src/test/java/projectHelper/WaitsGenerator.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class WaitsGenerator {
 
@@ -19,6 +19,12 @@ public class WaitsGenerator {
     public static WebElement waitForElementByXpath(WebDriver driver, By locator) {
         return waitMethod(driver).until(ExpectedConditions.presenceOfElementLocated(locator));
     }
+    public static WebElement waitForElement(WebDriver driver, WebElement element) {
+        return waitMethod(driver).until(ExpectedConditions.elementToBeClickable(element));
+    }
 
+    public static List<WebElement> waitForWebElements(WebDriver driver, List<WebElement> elements) {
+        return waitMethod(driver).until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
 
 }
