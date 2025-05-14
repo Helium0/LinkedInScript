3844bd0d148911bee119e338d8409540677a4cf9
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-14 23:32:23 +0200

    Refactored code

--- src/test/java/tests/PersonalizedInvitations.java
+++ src/test/java/tests/PersonalizedInvitations.java
@@ -1,47 +1,47 @@
 package tests;
 
-import driver.DriverFactory;
 import driver.DriverManagement;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-import projectHelper.AdditionalBrowserOptions;
+import pages.AdditionalUserMessagePage;
+import pages.NavigatorBarPage;
+import pages.PersonsFinderPage;
+import projectHelper.FileGenerator;
 import projectHelper.GenerateDate;
-
+import projectHelper.WaitsGenerator;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.time.Duration;
-import java.util.Date;
 import java.util.List;
 
 public class PersonalizedInvitations {
 
+    private NavigatorBarPage navigatorBarPage;
+    private PersonsFinderPage personsFinderPage;
+    private AdditionalUserMessagePage additionalUserMessagePage;
     private final By ALL_PERSONS_BUTTON = By.xpath("//a[text()='Zobacz wszystkie wyniki osób']");
     private final By PERSONS_LIST = By.xpath("//ul[@role='list']//li");
-
-
+    private int invitations = 1;
+    private final String TEXT_IN_SEARCH_BAR = "it hr";
 
     @Test
     public void multiple() throws InterruptedException, FileNotFoundException {
         String date = GenerateDate.actuallDate();
-        WebDriver driver = DriverFactory.getBrowser("chrome", AdditionalBrowserOptions.browserArguments());
-        DriverManagement.setDriver(driver);
-        driver = DriverManagement.getDriver();
-        driver.get("https://www.linkedin.com/");
+        WebDriver driver = DriverManagement.getDriver();
+        navigatorBarPage = new NavigatorBarPage(driver);
+        personsFinderPage = new PersonsFinderPage(driver);
+        additionalUserMessagePage = new AdditionalUserMessagePage(driver);
         WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        WebElement te = driver.findElement(By.xpath("//input[@placeholder='Szukaj']"));
-        te.sendKeys("it hr");
-        Actions actions = new Actions(driver);
-        actions.click(te).keyDown(Keys.ENTER).keyUp(Keys.ENTER).perform();
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(ALL_PERSONS_BUTTON)).click();
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(PERSONS_LIST));
-        PrintWriter write = new PrintWriter("invited_persons.txt");
+        navigatorBarPage.typeTextInToSearchBar(TEXT_IN_SEARCH_BAR);
+        navigatorBarPage.clickToExpandAllPersons();
+        WaitsGenerator.waitForElementByXpath(driver,ALL_PERSONS_BUTTON).click();
+        WaitsGenerator.waitForElementByXpath(driver,PERSONS_LIST);
+        PrintWriter write = FileGenerator.writerFile();
         write.println("Wysłanie spersonalizowanych zaproszeń w dniu: "+date);
         List<WebElement> persons = driver.findElements(By.xpath("//ul[@role='list']//li"));
-        int invitations = 1;
+
         for (int i = 0; i <= persons.size(); i++) {
             try {
 
@@ -54,49 +54,27 @@ public class PersonalizedInvitations {
                 write.println("Nazwa użytkownika: "+user.getText());
                 write.println("Zawód/Tagi: "+userBar.getText());
                 try {
-                    WebElement contactButton = element.findElement(By.xpath(".//span[text()='Nawiąż kontakt']"));
-                    contactButton.click();
-
-                    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
+                    personsFinderPage.clickToMakeConnectionWithPerson();
                     try {
-                        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                                By.xpath("//span[text()='Spersonalizuj zaproszenie dla użytkownika ']")));
-
-                        driver.findElement(By.xpath(".//button[@aria-label='Dodaj notatkę']")).click();
-
-                        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("custom-message")));
-
-                        WebElement text = driver.findElement(By.id("custom-message"));
-                        text.sendKeys("Hej " + user.getText() + "\n" +
-                                "przesyłam tę wiadomość w dniu: " + date + "\n" +
-                                "Używając skryptu w Java/Selenium, " +
-                                "moim celem było zautomatyzowanie wysyłania spersonalizowanych zaproszeń =)");
-//                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Wyślij zaproszenie']"))).click();
-                        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Odrzuć']"))).click();
+                        WaitsGenerator.waitForElement(driver,additionalUserMessagePage.visibleText());
+                        additionalUserMessagePage.addUserNoteButton();
+                        WaitsGenerator.waitForVisibilityElement(driver, additionalUserMessagePage.sendCustomUserMessage())
+                                .sendKeys("Hej " + user.getText() + "\n" +
+                                        "przesyłam tę wiadomość w dniu: " + date + "\n" +
+                                        "Używając skryptu w Java/Selenium, " +
+                                        "moim celem było zautomatyzowanie wysyłania spersonalizowanych zaproszeń =)");
+                        WaitsGenerator.waitForElement(driver, additionalUserMessagePage.clickOnCancelButton()).click();
                         write.println("Zaproszenie "+invitations+" wysłane =)");
                         if (invitations == 3) {
                             break;
                         }
 
                         invitations += 1;
-
-
                     } catch (TimeoutException e1) {
-
-                        List<WebElement> emailLabel = driver.findElements(
-                                By.xpath("//label[contains(., 'wpisz jego(jej) adres e-mail')]"));
-
-                        if (!emailLabel.isEmpty()) {
-                            WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(
-                                    By.xpath("//input[@name='email']")));
-                            emailInput.sendKeys("Nie znam adresu e-mail nie mogę wysłać zaproszenia =(");
-
-                            wait.until(ExpectedConditions.elementToBeClickable(
-                                    By.xpath("//button[@aria-label='Odrzuć']"))).click();
-                            write.println("Nie znam adresu e-mail aby wysłać zaproszenie");
+                        additionalUserMessagePage.usersEmailLabel("Nie znam adresu e-mail nie mogę wysłać zaproszenia =(");
+                        WaitsGenerator.waitForElement(driver, additionalUserMessagePage.clickOnCancelButton()).click();
+                        write.println("Nie znam adresu e-mail aby wysłać zaproszenie");
                         }
-                    }
 
                 } catch (NoSuchElementException | ElementNotInteractableException e) {
                     write.println("Osoba już jest w Twoich znajomych lub można było dodać tylko do obserwujących "
@@ -106,7 +84,6 @@ public class PersonalizedInvitations {
             } catch (StaleElementReferenceException | NoSuchElementException e) {
 
             }
-
         }
         write.close();
     }
